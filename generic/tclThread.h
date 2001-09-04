72de82de9eb47d84206a90e4d19b229e09d7dabd
davygrvy <davygrvy>
2001-09-04 23:43:09 +0000

    * generic/thread.h (deleted): 	* generic/tclThread.h (new): 	* generic/threadCmd.c:  decided to change the name of 'thread.h' to 	'tclThread.h', per request.

--- generic/tclThread.h
+++ generic/tclThread.h
@@ -7,7 +7,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: thread.h,v 1.11 2001/09/04 23:39:52 davygrvy Exp $
+ * RCS: @(#) $Id: tclThread.h,v 1.1 2001/09/04 23:43:09 davygrvy Exp $
  * ---------------------------------------------------------------------------
  */
 
