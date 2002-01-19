41b272082d4cd80074b129b177af55e60540c0ef
vasiljevic <zv@archiware.com>
2002-01-19 23:29:52 +0000

    Preparing the move to 2.4. cygwin build not tested yet. vc++ build not modified. Will leave it to David.

--- win/threadWin.c
+++ win/threadWin.c
@@ -1,12 +1,12 @@
-/* 
+/*
  * threadWin.c --
  *
- *	Windows specific aspects for the thread extension.
+ * Windows specific aspects for the thread extension.
  *
- *	see http://dev.ajubasolutions.com/doc/howto/thread_model.html
+ * see http://dev.activestate.com/doc/howto/thread_model.html
  *
- *	Some of this code is based on work done by Richard Hipp on behalf of
- *	Conservation Through Innovation, Limited, with their permission.
+ * Some of this code is based on work done by Richard Hipp on behalf of
+ * Conservation Through Innovation, Limited, with their permission.
  *
  * Copyright (c) 1998 by Sun Microsystems, Inc.
  * Copyright (c) 1999,2000 by Scriptics Corporation.
@@ -14,19 +14,18 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: threadWin.c,v 1.1 2001/09/05 23:01:12 davygrvy Exp $
+ * RCS: @(#) $Id: threadWin.c,v 1.2 2002/01/19 23:29:52 vasiljevic Exp $
  */
 
-#include "tclThread.h"
+#include "../..generic/tclThread.h"
 #include <windows.h>
 #include <process.h>
 
-void
-ThreadKill (long id)
-{
-    HANDLE hThread = 0;
+/* EOF $RCSfile: threadWin.c,v $ */
 
-    /* get handle from id here... how? */
-
-    TerminateThread(hThread, 666);
-}
+/* Emacs Setup Variables */
+/* Local Variables:      */
+/* mode: C               */
+/* indent-tabs-mode: nil */
+/* c-basic-offset: 4     */
+/* End:                  */
