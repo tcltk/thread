d1da8bbb345a99f6b2c4bacb1215df55fc1ea367
vasiljevic <zv@archiware.com>
2002-01-19 23:15:47 +0000

    Prepared for 2.4 move.

--- generic/tclThread.h
+++ generic/tclThread.h
@@ -1,85 +1,72 @@
 /*
  * --------------------------------------------------------------------------
- * thread.h --
+ * tclthread.h --
  *
- * 	Global header file for the thread extension.
+ * Global header file for the thread extension.
  * 
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclThread.h,v 1.2 2001/09/05 23:02:01 davygrvy Exp $
+ * RCS: @(#) $Id: tclThread.h,v 1.3 2002/01/19 23:15:47 vasiljevic Exp $
  * ---------------------------------------------------------------------------
  */
 
 /*
- * thread extension version numbers are not stored here because this
- * isn't a public export file.
+ * Thread extension version numbers are not stored here
+ * because this isn't a public export file.
  */
 
 #ifndef _TCL_THREAD_H_
 #define _TCL_THREAD_H_
 
 #include <tcl.h>
+#include <string.h> /* For memset and friends */
 
-#ifdef __WIN32__
-#include <string.h>
-#endif
-
-/*
- * Functions from threadCmd.c file.
- */
-
-#undef TCL_STORAGE_CLASS
+#undef  TCL_STORAGE_CLASS
 #define TCL_STORAGE_CLASS DLLEXPORT
 
-EXTERN int	Thread_Init _ANSI_ARGS_((Tcl_Interp *interp));
-EXTERN int	Thread_SafeInit _ANSI_ARGS_((Tcl_Interp *interp));
-
-Tcl_ObjCmdProc	ThreadCreateObjCmd;
-Tcl_ObjCmdProc	ThreadSendObjCmd;
-Tcl_ObjCmdProc	ThreadJoinObjCmd;
-Tcl_ObjCmdProc	ThreadUnwindObjCmd;
-Tcl_ObjCmdProc	ThreadExitObjCmd;
-Tcl_ObjCmdProc	ThreadKillObjCmd;
-Tcl_ObjCmdProc	ThreadWaitObjCmd;
-Tcl_ObjCmdProc	ThreadIdObjCmd;
-Tcl_ObjCmdProc	ThreadNamesObjCmd;
-Tcl_ObjCmdProc	ThreadErrorProcObjCmd;
-Tcl_ObjCmdProc	ThreadTransferObjCmd;
-Tcl_ObjCmdProc	ThreadExistsObjCmd;
-
 /*
- * Functions from threadSvCmd.c file.
+ * Exported from threadCmd.c file.
  */
 
-Tcl_ObjCmdProc	ThreadSvSetObjCmd;
-Tcl_ObjCmdProc	ThreadSvGetObjCmd;
-Tcl_ObjCmdProc	ThreadSvIncrObjCmd;
-Tcl_ObjCmdProc	ThreadSvAppendObjCmd;
-Tcl_ObjCmdProc	ThreadSvArrayObjCmd;
-Tcl_ObjCmdProc	ThreadSvUnsetObjCmd;
-void	Initialize_Sv _ANSI_ARGS_((Tcl_Interp *interp));
+EXTERN int  Thread_Init     _ANSI_ARGS_((Tcl_Interp *interp));
+EXTERN int  Thread_SafeInit _ANSI_ARGS_((Tcl_Interp *interp));
 
 /*
- * Functions from threadSpCmd.c file.
+ * Exported from threadSvCmd.c file.
  */
 
-Tcl_ObjCmdProc	ThreadMutexObjCmd;
-Tcl_ObjCmdProc	ThreadCondObjCmd;
-void	Initialize_Sp _ANSI_ARGS_((Tcl_Interp *interp));
+EXTERN void Sv_Init _ANSI_ARGS_((Tcl_Interp *interp));
+
+/*
+ * Exported from threadSpCmd.c file.
+ */
+
+EXTERN void Sp_Init _ANSI_ARGS_((Tcl_Interp *interp));
 
 /*
  *  Platform specific functions.
  */
 
-void ThreadKill _ANSI_ARGS_((long id));
-
+EXTERN void ThreadKill _ANSI_ARGS_((long id));
 
 /*
- * end of header
- * reset TCL_STORAGE_CLASS to DLLIMPORT.
- */
-#undef TCL_STORAGE_CLASS
+ * Utility macros
+ */ 
+
+#define TCL_CMD(a,b,c) \
+  Tcl_CreateObjCommand((a), (b), (c), (ClientData)NULL, NULL);
+
+#define OPT_CMP(a,b) \
+  ((a) && (b) && (*(a)==*(b)) && (*(a+1)==*(b+1)) && (!strcmp((a),(b))))
+
+#ifndef TCL_TSD_INIT
+#define TCL_TSD_INIT(keyPtr) \
+  (ThreadSpecificData*)Tcl_GetThreadData((keyPtr),sizeof(ThreadSpecificData))
+#endif
+
+
+#undef  TCL_STORAGE_CLASS
 #define TCL_STORAGE_CLASS DLLIMPORT
 
 #endif /* _TCL_THREAD_H_ */
