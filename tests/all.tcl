ca2faa5dc05240d940d893a51fa9e178b09c8d5f
hobbs <hobbs>
2002-03-20 01:31:44 +0000

    improved to detect 8.3/8.4 pkg differences

--- tests/all.tcl
+++ tests/all.tcl
@@ -7,7 +7,7 @@
 # Copyright (c) 1998-1999 by Scriptics Corporation.
 # All rights reserved.
 # 
-# RCS: @(#) $Id: all.tcl,v 1.2 2000/04/17 20:37:30 welch Exp $
+# RCS: @(#) $Id: all.tcl,v 1.3 2002/03/20 01:31:44 hobbs Exp $
 
 package require tcltest
 namespace import -force ::tcltest::*
@@ -40,10 +40,17 @@ puts stdout "Tests began at [eval $timeCmd]"
 
 # These tests need to know which is the main thread
 
-package require Thread 2.0
+# Require the accurate version for the tests so we don't pick up
+# older Thread packages by accident
+if {$::tcl_version == 8.3} {
+    package require Thread 2.1.4
+} else {
+    package require Thread 2.4
+}
 set ::tcltest::mainThread [thread::id]
 
-puts stdout "Mainthread is  $::tcltest::mainThread"
+puts stdout "Thread [package provide Thread]"
+puts stdout "Mainthread id is $::tcltest::mainThread"
 
 # source each of the specified tests
 foreach file [lsort [::tcltest::getMatchingFiles]] {
