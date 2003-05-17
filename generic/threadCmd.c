f53acf35a7fdc44167a4ab1167a4082054cdc0c0
vasiljevic <zv@archiware.com>
2003-05-17 15:42:38 +0000

    Sets the name of the new threa dto "-tclthreead-" when compiled for AOLserver

--- generic/threadCmd.c
+++ generic/threadCmd.c
@@ -17,7 +17,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: threadCmd.c,v 1.74 2003/04/10 14:05:48 vasiljevic Exp $
+ * RCS: @(#) $Id: threadCmd.c,v 1.75 2003/05/17 15:42:39 vasiljevic Exp $
  * ----------------------------------------------------------------------------
  */
 
@@ -412,14 +412,14 @@ Thread_Init(interp)
      * Add shared variable commands
      */
     
-    Sv_Init(interp);
+    //Sv_Init(interp);
     
     /*
      * Add commands to access thread
      * synchronization primitives.
      */
     
-    Sp_Init(interp);
+    //Sp_Init(interp);
 
     /*
      * Add threadpool commands.
@@ -1500,6 +1500,7 @@ NewThread(clientData)
 
 #ifdef NS_AOLSERVER
     struct mydata *md = (struct mydata*)ctrlPtr->cd;
+    Ns_ThreadSetName("-tclthread-");
     interp = (Tcl_Interp*)Ns_TclAllocateInterp(md ? md->server : NULL);
 #else
     interp = Tcl_CreateInterp();
