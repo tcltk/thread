e50afb42e51d0f24b3d8eece067631aa09d4d2aa
dgp <dgp@users.sourceforge.net>
2015-01-23 18:30:48 +0000

    Bump to Thread 2.7.2

--- lib/ttrace.tcl
+++ lib/ttrace.tcl
@@ -73,7 +73,7 @@ namespace eval ttrace {
     }
 
     # Keep in sync with the Thread package
-    package provide Ttrace 2.7.1
+    package provide Ttrace 2.7.2
 
     # Package variables
     variable resolvers ""     ; # List of registered resolvers
