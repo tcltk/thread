720f725d6e60f74e66b774d467f6d3b3ece274ee
gahr <gahr@gahr.ch>
2016-06-01 11:18:21 +0000

    2.7.3 -> 2.8.0

--- generic/threadCmd.c
+++ generic/threadCmd.c
@@ -27,7 +27,7 @@
  * files built as part of that shell. Example: basekits.
  */
 #ifndef PACKAGE_VERSION
-#define PACKAGE_VERSION "2.7.3"
+#define PACKAGE_VERSION "2.8.0"
 #endif
 
 /*
--- lib/ttrace.tcl
+++ lib/ttrace.tcl
@@ -73,7 +73,7 @@ namespace eval ttrace {
     }
 
     # Keep in sync with the Thread package
-    package provide Ttrace 2.7.3
+    package provide Ttrace 2.8.0
 
     # Package variables
     variable resolvers ""     ; # List of registered resolvers
