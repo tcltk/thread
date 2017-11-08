db5d717bf5b4589058c0a0b991227b2bb78d3b9e
jan.nijtmans <nijtmans@users.sourceforge.net>
2017-11-08 09:17:14 +0000

    Update Thread version number to 2.8.2

--- lib/ttrace.tcl
+++ lib/ttrace.tcl
@@ -73,7 +73,7 @@ namespace eval ttrace {
     }
 
     # Keep in sync with the Thread package
-    package provide Ttrace 2.8.1
+    package provide Ttrace 2.8.2
 
     # Package variables
     variable resolvers ""     ; # List of registered resolvers
