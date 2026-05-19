af59c677c7238d065890af20ca3648b311ac5dca
apnadkarni <apnmbx-wits@yahoo.com>
2026-05-19 04:59:50 +0000

    Bump to 3.0.6

--- tests/all.tcl
+++ tests/all.tcl
@@ -9,7 +9,7 @@
 
 package require tcltest
 ::tcltest::loadTestedCommands
-package require -exact thread 3.0.5
+package require -exact thread 3.0.6
 
 set ::tcltest::testSingleFile false
 set ::tcltest::testsDirectory [file dir [info script]]
