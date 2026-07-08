15220fd1cf164acad1b5852d2ee1965295d90511
jan.nijtmans <nijtmans@users.sourceforge.net>
2026-07-08 10:34:30 +0000

    Version -> 3.0.7, prepare for next release

--- tests/all.tcl
+++ tests/all.tcl
@@ -9,7 +9,7 @@
 
 package require tcltest
 ::tcltest::loadTestedCommands
-package require -exact thread 3.0.6
+package require -exact thread 3.0.7
 
 set ::tcltest::testSingleFile false
 set ::tcltest::testsDirectory [file dir [info script]]
